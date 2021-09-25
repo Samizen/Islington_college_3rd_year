aeb1148074f2b57595b084360c4be64b5358cd98
Samizen <samitpdl@gmail.com>
2021-09-25 21:01:39 +0545

    Add Files for Application Development, Artifitial Intelligence and Advanced Database for First Week

---
+++ Application Development/Week_1/Week_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(10.0, 20.0);
+            rect.Length = 22.0;
+            double area = rect.GetArea();
+            Console.WriteLine("Area of Rectangle: {0}", area);
+        }
+    }
+}
---
+++ Application Development/Week_1/Week_1/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Week_1
+{
+    // Defining a Class
+    class Rectangle
+    {
+        public double length;
+        public double width;
+
+        // Constructor
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        // Properties:
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                if (value > 0.0)
+                {
+                    length = value;
+                }
+            }
+        }
+    }
+}
